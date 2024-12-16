51234735aa415693e61eb16ac17902c42637ce33
Aidan Nagorcka-Smith <aidanns@gmail.com>
2024-12-16 21:57:46 +1100

    Bootstrap new C++ project with testing and benchmarks

---
+++ src/cpp/test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <nanobench.h>
+
+namespace AdventOfCode::DayZero {
+    TEST(DayZero, placeholder) {
+        EXPECT_EQ(0, 0);
+    }
+
+    TEST(DayZero, placeholderBenchmark) {
+        ankerl::nanobench::Bench().run("Day 0 Placeholder", [&]() {});
+    }
+
+} // AdventOfCode
